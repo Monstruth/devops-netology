fe72a32292648582b39a9fd2ce2897ba7439d1ff
perepelitsyn.alexei <lehakaluga@rambler.ru>
2023-04-26 18:38:19 +0300

    update readmi on paCharm

--- terraform/src/variables.tf
+++ terraform/src/variables.tf
@@ -36,6 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDY7TuKDvoZAcFEUMzHSAFhLXcGlr5nvG45h/emkpJx2 monztro@monztro-laptop"
   description = "ssh-keygen -t ed25519"
 }
