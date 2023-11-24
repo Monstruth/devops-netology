3070adbd1c9e6de89ce31c8bb4d9a78919f2e6f2
perepelitsyn.alexei <lehakaluga@rambler.ru>
2023-11-24 20:08:45 +0300

    update readmi on paCharm

--- ter-homeworks-03/src/count-vm.tf
+++ ter-homeworks-03/src/count-vm.tf
@@ -33,6 +33,7 @@ resource "yandex_compute_instance" "example" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
   }
   allow_stopping_for_update = true
 }
