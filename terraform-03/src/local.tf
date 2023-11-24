2cfab81e067b33afe779b7a93779b42a5baf777f
perepelitsyn.alexei <lehakaluga@rambler.ru>
2023-11-25 00:27:55 +0300

    update readmi on paCharm

--- terraform-03/src/local.tf
+++ terraform-03/src/local.tf
@@ -1,15 +1,20 @@
+resource "null_resource" "web_hosts_provision" {
+#Ждем создания инстанса
 depends_on = [yandex_compute_instance.example]
 
-
+#Добавление ПРИВАТНОГО ssh ключа в ssh-agent
   provisioner "local-exec" {
     command = "cat ~/.ssh/id_ed25519 | ssh-add -"
   }
 
-provisioner "local-exec" {
+#Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
+# В случае использования cloud-init может потребоваться еще больше времени
+ provisioner "local-exec" {
     command = "sleep 60"
   }
 
- provisioner "local-exec" {
+#Запуск ansible-playbook
+  provisioner "local-exec" {
     command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
     on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
     environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
@@ -20,3 +25,5 @@ provisioner "local-exec" {
       playbook_src_hash  = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
       ssh_public_key     = var.public_key # при изменении переменной
     }
+
+}
