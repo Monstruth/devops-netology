1d2de1641bcdf68fb133bfc9762de92ae53efc9a
perepelitsyn.alexei <lehakaluga@rambler.ru>
2023-11-25 02:15:00 +0300

    update readmi on paCharm

--- terraform-03/src/disk_vm.tf
+++ terraform-03/src/disk_vm.tf
@@ -29,6 +29,7 @@ resource "yandex_compute_instance" "storage" {
 
   network_interface {
     subnet_id          = yandex_vpc_subnet.develop.id
+    nat       = true
   }
 
   metadata = local.ssh_keys_and_serial_port
